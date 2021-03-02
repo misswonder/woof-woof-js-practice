2833fd211300b7f57be0c2c7dda79a51fbafdfb7
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-02 11:50:08 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 const baseURL = "http://localhost:3000/pups";
 let allDogs;
+let filter = false;
 
 // add event listern for the DOM
 document.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("good-dog-filter")
-    .addEventListener("click", (event) => {
-      const currentFilter = event.target.innerText === "Filter good dogs: ON";
-      const filter = !currentFilter;
-      const elements = document.getElementById("dog-bar").children;
-
-      if (filter) {
-        event.target.innerText = "Filter good dogs: ON";
-        for (element of elements) {
-          if (allDogs.find((dog) => dog.id === element.dogId)?.isGoodDog) {
-            element.style.display = "flex";
-          } else {
-            element.style.display = "none";
-          }
-        }
-      } else {
-        event.target.innerText = "Filter good dogs: OFF";
-        for (element of elements) {
-          element.style.display = "flex";
-        }
-      }
+    .addEventListener("click", () => {
+      filter = !filter;
+      filterGoodDogs();
     });
 
   getDogs();
 });
+
+function filterGoodDogs() {
+    const elements = document.getElementById("dog-bar").children;
+    const filterButton = document.getElementById("good-dog-filter")
+
+    if (filter) {
+      filterButton.innerText = "Filter good dogs: ON";
+      for (element of elements) {
+        if (allDogs.find((dog) => dog.id === element.dogId)?.isGoodDog) {
+          element.style.display = "flex";
+        } else {
+          element.style.display = "none";
+        }
+      }
+    } else {
+      filterButton.innerText = "Filter good dogs: OFF";
+      for (element of elements) {
+        element.style.display = "flex";
+      }
+    }
+}
 
 // fetch all dogs function
 function getDogs() {
@@ -36,48 +41,53 @@ function getDogs() {
     .then((res) => res.json())
     .then((dogData) => {
       allDogs = dogData;
-      dogData.forEach(renderDogs);
+      dogData.forEach(addDog);
     });
 }
 
-// render dog function
-function renderDogs(dog) {
+// add dog function
+function addDog(dog) {
   let span = document.createElement("span");
   span.innerText = dog.name;
   span.dogId = dog.id;
-
-  let h2 = document.createElement("h2");
-  h2.innerText = dog.name;
-
-  let img = document.createElement("img");
-  img.src = dog.image;
-
-  //   let isGoodDog = dog.isGoodDog;
-  let btn = document.createElement("button");
-
-  const setGoodDog = () => {
-    // if (isGoodDog) {
-    if (dog.isGoodDog) {
-      btn.innerText = "Good Dog!";
-    } else {
-      btn.innerText = "Bad Dog";
-    }
-  };
-
-  setGoodDog();
-
-  btn.addEventListener("click", () => {
-    debugger;
-    // isGoodDog = !isGoodDog;
-    dog.isGoodDog = !dog.isGoodDog;
-    setGoodDog();
-    // updatedDog(dog.id, isGoodDog);
-    updatedDog(dog.id, dog.isGoodDog);
+  span.addEventListener('click', () => {
+      showDog(dog);
   });
 
   document.querySelector("#dog-bar").appendChild(span);
-  document.querySelector("#dog-info").append(img, h2, btn);
 }
+
+function showDog(dog) {
+    let h2 = document.createElement("h2");
+    h2.innerText = dog.name;
+  
+    let img = document.createElement("img");
+    img.src = dog.image;
+  
+    //   let isGoodDog = dog.isGoodDog;
+    let btn = document.createElement("button");
+  
+    const setGoodDog = () => {
+      // if (isGoodDog) {
+      if (dog.isGoodDog) {
+        btn.innerText = "Good Dog!";
+      } else {
+        btn.innerText = "Bad Dog";
+      }
+    };
+  
+    setGoodDog();
+  
+    btn.addEventListener("click", () => {
+      dog.isGoodDog = !dog.isGoodDog;
+      setGoodDog();
+      updatedDog(dog.id, dog.isGoodDog);
+      filterGoodDogs();
+    });
+  
+    document.getElementById("dog-info").innerHTML = '';
+    document.querySelector("#dog-info").append(img, h2, btn);
+  }
 
 function updatedDog(dogID, isGoodDog) {
   const body = { isGoodDog: isGoodDog };
